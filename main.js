4701d59c8703732a775324eced69153ce904b490
yousukeayada <yousukeayada@gmail.com>
2021-10-25 19:19:02 +0900

    add mock

---
+++ main.js
@@ -0,0 +1,11 @@
+function moveHome() {
+    location.href = "index.html";
+}
+
+function moveSight() {
+    location.href = "sight.html";
+}
+
+function moveFeel() {
+    location.href = "feel.html";
+}
