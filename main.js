4d7777f89d4cee74b9d341937f6c6462ff8811d3
yousukeayada <yousukeayada@gmail.com>
2021-10-27 19:30:04 +0900

    tmp

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let anxiety = "";
+let anxietyDegree = "";
+
 function moveHome() {
     location.href = "index.html";
 }
@@ -8,4 +11,23 @@ function moveSight() {
 
 function moveFeel() {
     location.href = "feel.html";
+}
+
+function homeToFive() {
+    anxiety = document.getElementById("anxiety").value;
+    anxietyDegree = document.getElementById("anxiety-degree").value;
+
+    console.log(anxiety);
+    if(anxiety == "") {
+        alert("入力してください");
+        return;
+    }
+
+    moveSight();
+}
+
+function fiveToHome() {
+    // document.getElementById("anxiety").textContent = anxiety;
+    console.log(anxiety);
+    moveHome();
 }
